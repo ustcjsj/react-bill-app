8e46124fce312e0d6c67f561750e4ca44761ac9c
Jiang Shunjia <ustcjsj@ustc.edu>
2024-04-05 17:11:54 +0800

    定制主题色

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,7 +1,12 @@
-import {Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import {Button} from 'antd-mobile';
 const Layout = () => {
   return <div>
-    <h1>Layout</h1>
+    <Button color="primary">Primary</Button>
+    <div className="purple">
+      <Button color="primary" >jubu</Button>
+    </div>
+    <h1>Layout</h1>]
     <Outlet />
   </div>
 }
